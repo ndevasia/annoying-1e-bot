83f89fcacea143a9d1d236cce3815117d0c254be
ndevasia <35207687+ndevasia@users.noreply.github.com>
2020-03-17 01:03:10 -0400

    manually updating because i am afraid

--- bot.js
+++ bot.js
@@ -1,4 +1,6 @@
 var Discord = require('discord.io');
+var Discord_voice = require('discord.js');
+const client = new Discord_voice.Client();
 var logger = require('winston');
 var auth = require('./auth.json');
 // Configure logger settings
@@ -54,4 +56,23 @@ bot.on('message', function (user, userID, channelID, message, evt) {
         }
 
      }
+});
+
+client.login('Njg5MzExNDM5MTA5ODgxODY3.XnBBgw.xAl_R3ELYVBja02bZI2RvTdamC8');
+
+client.on('message', async message => {
+  // Voice only works in guilds, if the message does not come from a guild,
+  // we ignore it
+  if (!message.guild) return;
+
+  if (message.content === 'the last ship') {
+    // Only try to join the sender's voice channel if they are in one themselves
+    if (message.member.voice.channel) {
+      const connection = await message.member.voice.channel.join();
+      const ytdl = require('ytdl-core');
+      connection.play(ytdl('https://www.youtube.com/watch?v=VOmUw6Y_z6I', { filter: 'audioonly' }));
+    } else {
+      message.reply('You need to join a voice channel first!');
+    }
+  }
 });
